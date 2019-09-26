f9a4ba55c4598f714ef1fa2d59ad91160a0236cc
sebastosh <sebastosh@protonmail.com>
2019-09-26 17:27:46 -0400

    new gatsby site

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,25 @@
  */
 
 module.exports = {
-  /* Your site config here */
+    siteMetadata: {
+    title: `Sebastien Sanz de Santamaria`,
+  },
+  
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
